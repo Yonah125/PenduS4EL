020ec7e33dc6ccf4045777734aef3861cc22c0e2
Yonah125 <yonah@sfr.fr>
2020-02-13 21:32:09 +0100

    V1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,123 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Keyboard from "./Keyboard";
+
+const MAX_ERRORS = 7
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        // here I tried to call initialize which contained a setState, but I got a
+        // "Can't call setState on a component that is not yet mounted" message
+        this.state = {
+            words: [''],
+            toGuess: '',
+            toDisplay: [''],
+            numberOfAttempts: 0,
+            errors: 0
+        }
+    }
+
+   componentDidMount() {
+	this.setState ({words:["YONAH","DENIS","PIERRE","MANON","ANTONY"]}, () => {this.initialize();});
+   	
+    }
+
+    initialize() {
+        const IDX_OF_WORD_TO_GUESS = Math.floor(Math.random() * Math.floor(this.state.words.length));
+        let toDisplay = []
+        for (let idx = 0; idx < this.state.words[IDX_OF_WORD_TO_GUESS].length; idx++) {
+            toDisplay[idx] = "_";
+        }
+        this.setState ({
+            toGuess: this.state.words[IDX_OF_WORD_TO_GUESS],
+            toDisplay: toDisplay,
+            numberOfAttempts: 0,
+            errors: 0
+        })
+    }
+
+    // called in an onClick of a button, so a binding is mandatory
+    // to access the state
+    playAgain = () => {
+        this.initialize();
+    }
+
+    displayWord() {
+        // In the following, arrayIndex used to provide a key prop, to avoid warning
+        return this.state.toDisplay.map(
+            (keyClicked, arrayIndex) => <span key={arrayIndex} className="oneLetter">{keyClicked}</span>)
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <header>
+                    <img src={require("./assets/ReactLogo.svg")} height="190px" alt={"react logo"}/>
+                </header>
+                <div className="App-header">
+                    <p className="picture">
+                    {this.state.errors > 0?
+                    <img src={require("./assets/pendu"+this.state.errors+".png")} alt="hangman"/>:null
+                    }
+                    </p>
+                    <p>
+                        {this.displayWord()}
+                    </p>
+                    <p>#attempt: {this.state.numberOfAttempts}</p>
+                </div>
+                {this.playerHasLost()?
+                    <div className="App-sorry">
+                        <input type="button" value="OMG! You're dead! Click to play again" onClick={this.playAgain}/>
+                    </div>
+                    :
+                    this.state.toDisplay.includes("_") ?
+                        <Keyboard onClickFunction={this.tryLetter}/>
+                        :
+                        <div className="App-congrats">
+                            <input type="button" value="Congratulations! Click to play again" onClick={this.playAgain}/>
+                        </div>
+                }
+            </div>
+        )
+    }
+
+  playerHasLost() {
+      return this.state.errors === MAX_ERRORS
+  }
+
+  // called in an onClick of a button, so a binding is mandatory
+  // to access the state
+  tryLetter = (keyClicked) => {
+
+        let {errors, numberOfAttempts, toDisplay, toGuess} = this.state
+
+        numberOfAttempts++
+
+        let baseIdx = 0
+        let found = false;
+        let foundSomething = false
+        do {
+            let idx = toGuess.indexOf(keyClicked, baseIdx)
+            if (idx >= 0) {
+                foundSomething = true
+                found = true
+                toDisplay[idx] = keyClicked;
+                baseIdx = idx + 1
+            } else {
+                found = false
+            }
+        } while (found)
+        if (!foundSomething) {
+            errors++
+        }
+        this.setState({toDisplay: toDisplay, numberOfAttempts: numberOfAttempts, errors: errors})
+    }
 }
 
 export default App;
---
+++ src/Keyboard.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class Keyboard extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            keys: this.createArray(),
+            onClick:props.onClickFunction,
+            disabledLetters: []
+        }
+    }
+
+    createArray() {
+        let keys = []
+        for (let idx = 65; idx < 65 + 26; idx++) {
+            keys[idx - 65] = idx
+        }
+
+        return keys;
+    }
+
+    isKeyDisabled(keyClicked) {
+        return this.state.disabledLetters.includes(keyClicked)
+    }
+
+    setKeyDisabled(keyClicked)  {
+        this.state.disabledLetters.push(keyClicked)
+    }
+
+    render() {
+
+        // In the following, arrayIndex used to provide a key prop, to avoid warning
+        return (
+            <div className="App-keyboard">
+                {
+                    this.state.keys.map(
+                        (keyCode, arrayIndex) =>
+
+                        <input key={arrayIndex}
+                               type="button"
+                               value={String.fromCharCode(keyCode)}
+                               disabled={this.isKeyDisabled(keyCode)}
+                               onClick={() =>
+                               {
+                                   this.setKeyDisabled(keyCode)
+                                   this.state.onClick(String.fromCharCode(keyCode))
+                               }}/>)
+                }
+            </div>
+        )
+    }
+}
+
+export default Keyboard
